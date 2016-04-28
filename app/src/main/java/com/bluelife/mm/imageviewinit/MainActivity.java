d7fae122bec28ab136c21e6c91f254418e599a05
bluelife <28998638@qq.com>
2016-04-28 10:42:11 +0800

    Initial commit

---
+++ app/src/main/java/com/bluelife/mm/imageviewinit/MainActivity.java
@@ -0,0 +1,20 @@
+package com.bluelife.mm.imageviewinit;
+
+import android.content.Context;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView=new ImageView(context);
+    }
+}
